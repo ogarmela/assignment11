feb467413f5cfd95668a4ee5ae24415a17f76bfb
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-03-22 11:27:31 -0700

    Add files via upload

---
+++ fadumo1.java
@@ -0,0 +1,22 @@
+package assignment08;
+
+public class fadumo1 {
+
+	public int getHours() {
+        return 2 * super.getHours();
+    }
+    
+    public double getSalary() {
+        return super.getSalary() - 10000;
+    }
+    
+    public int getVacationDays() {
+        return super.getVacationDays() / 2;
+    }
+    
+    public void clean() {
+        System.out.println("Workin' for the man.");
+    }
+
+
+}
